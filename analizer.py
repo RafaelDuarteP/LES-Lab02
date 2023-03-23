e5f579100fde599f18d666791f2db0f15835fc0f
Rafael Duarte Pereira <1296852@sga.pucminas.br>
2023-03-22 22:07:36 -0300

    todos os graficos

--- analizer.py
+++ analizer.py
@@ -6,8 +6,15 @@ from datetime import datetime
 
 df =  pd.read_csv('dados-repo.csv')
 
+#removendo os dados vazios
 df = df.loc[df['visited'] == True]
 df = df.dropna(subset=['dit'])
 
-sns.pairplot(data=df, y_vars='stars', x_vars='lcom', kind="reg",height=10)
-plt.show()
+x_axis = ['stars', 'age', 'releases', 'loc']
+y_axis = ['dit', 'cbo', 'lcom']
+
+for x in x_axis:
+    for y in y_axis:
+        sns.pairplot(data=df, y_vars=y, x_vars=x, kind="reg",height=10)
+        plt.show()
+        
