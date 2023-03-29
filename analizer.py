1a32fc3ff97cc105f9fe77b1c314e647d4634ec3
Rafael Duarte Pereira <1296852@sga.pucminas.br>
2023-03-28 21:13:24 -0300

    Mudança no teste de correlação

--- analizer.py
+++ analizer.py
@@ -12,14 +12,14 @@ x_axis = ['stars', 'age', 'releases', 'loc']
 y_axis = ['dit', 'cbo', 'lcom']
 
 for x in x_axis:
-    for y in y_axis:
-        print('Relação', x, y)
-        sns.pairplot(data=df, y_vars=y, x_vars=x, kind="reg", height=10)
-        plt.show()
-        pd.plotting.scatter_matrix(df[[x, y]], diagonal='kde')
-        plt.show()
+    # for y in y_axis:
+    #     print('Relação', x, y)
+    #     sns.pairplot(data=df, y_vars=y, x_vars=x, kind="reg", height=10)
+    #     plt.show()
+    #     pd.plotting.scatter_matrix(df[[x, y]], diagonal='kde')
+    #     plt.show()
     cols = [x] + y_axis
-    corr = df[cols].corr()
+    corr = df[cols].corr('spearman')
     print(corr)
     plt.matshow(corr)
     plt.xticks(range(len(cols)), cols)
